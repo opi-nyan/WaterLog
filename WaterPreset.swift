0fe8160ce7fd033eccb1c33bb1f47ee3adfbba08
opi-nyan <208742032+opi-nyan@users.noreply.github.com>
2025-04-28 13:19:32 +0900

    main

--- WaterPreset.swift
+++
@@ -1,8 +0,0 @@
-import Foundation // UUIDやCodableのために必要
-
-// 水分摂取パターンを表す構造体（設計図）
-struct WaterPreset: Identifiable, Codable {
-    let id = UUID()      // 一つ一つのパターンを区別するためのユニークなID
-    var name: String     // パターンの名前 (例: "コップ")
-    var amount: Double   // パターンの量 (例: 200.0 ml)
-}
